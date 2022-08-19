6b8b2580369aee34e5c3e680c875d28884eb5b08
nikolaitur <n.turovskyi@gmail.com>
2022-08-18 17:56:37 -0700

    wip fix sections

--- components/Sections/ContentSections/ContentSections.js
+++ components/Sections/ContentSections/ContentSections.js
@@ -17,30 +17,30 @@ const ContentSections = ({ sections }) => {
             {sections.map((section, index) => {
                 const type = section.type
         
-                // switch (type) {
-                //     case 'heroBanner':
-                //         return <HeroBanner content={section} key={index} />
-                //     case 'contentBanner':
-                //         return <ContentBanner content={section} key={index} />
-                //     case 'categoryCards':
-                //         return <CollectionCards content={section} key={index} />
-                //     case 'diaperFinder':
-                //         return <DiaperFinder content={section} key={index} />
-                //     case 'shopByCategory':
-                //         return <ShopByCategory content={section} key={index} />
-                //     case 'bestSellers':
-                //             return <BestSellers content={section} key={index} />
-                //     case 'storiesSlider':
-                //         return <StoriesSlider content={section} key={index} />
-                //     case 'contentPreview':
-                //         return <ContentPreview content={section} key={index} />
-                //     case 'wowAndHow':
-                //         return <WowAndHow content={section} key={index} />
-                //     case 'about':
-                //         return <About content={section} key={index} />
-                //     default:
-                //     return null
-                // }
+                switch (type) {
+                    case 'heroBanner':
+                        return <HeroBanner content={section} key={index} />
+                    case 'contentBanner':
+                        return <ContentBanner content={section} key={index} />
+                    case 'categoryCards':
+                        return <CollectionCards content={section} key={index} />
+                    case 'diaperFinder':
+                        return <DiaperFinder content={section} key={index} />
+                    case 'shopByCategory':
+                        return <ShopByCategory content={section} key={index} />
+                    case 'bestSellers':
+                            return <BestSellers content={section} key={index} />
+                    case 'storiesSlider':
+                        return <StoriesSlider content={section} key={index} />
+                    case 'contentPreview':
+                        return <ContentPreview content={section} key={index} />
+                    case 'wowAndHow':
+                        return <WowAndHow content={section} key={index} />
+                    case 'about':
+                        return <About content={section} key={index} />
+                    default:
+                    return null
+                }
             })}
         </>
     )
