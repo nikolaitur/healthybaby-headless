b50243272e0070070b253b4576bc40a3f557843b
nikolaitur <n.turovskyi@gmail.com>
2022-09-09 16:18:09 -0700

    wip adding content

--- components/Sections/ProductDetailTabs/ProductDetailTabs.js
+++ components/Sections/ProductDetailTabs/ProductDetailTabs.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
 import ProductDetailSlide from './ProductDetailSlide';
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { EffectFade, Navigation, Pagination } from "swiper";
@@ -10,17 +8,18 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
 const ProductDetailTabs = ({ content }) => {
-   const { header, subheader, sections } = content.fields
-   console.log(content)
+    const { header, subheader, sections } = content.fields
 
-   const [swiperIntstance, setSwiperInstance] = useState(null)
-   const [activeSlide, setActiveSlide] = useState(0);
+    const [swiperIntstance, setSwiperInstance] = useState(null)
+    const [activeSlide, setActiveSlide] = useState(0);
 
-   const goToSlide = (index) => {
+    const goToSlide = (index) => {
        swiperIntstance.slideTo(index)
        setActiveSlide(index)
-   }
+    }
 
     return (
         <section className="product-detail-tabs">
@@ -32,38 +31,35 @@ const ProductDetailTabs = ({ content }) => {
                     {content.fields?.header ? (
                         <h2 className="product-detail-tabs__header">{ content.fields.header }</h2>
                     ) : ""}
-                    <div className="product-detail-tabs__tabs">
-                        <div className={`product-detail-tabs__tab ${0 == activeSlide ? "active" : ""}`} onClick={() => goToSlide(0)}>Preconception + Trimester 1</div>
-                        <div className={`product-detail-tabs__tab ${1 == activeSlide ? "active" : ""}`} onClick={() => goToSlide(1)}>Trimester 2</div>
-                        <div className={`product-detail-tabs__tab ${2 == activeSlide ? "active" : ""}`} onClick={() => goToSlide(2)}>Trimester 3</div>
-                        <div className={`product-detail-tabs__tab ${3 == activeSlide ? "active" : ""}`} onClick={() => goToSlide(3)}>Postnatal + Trimester 4</div>
-                    </div>
+                    {content.fields?.sections ? (
+                        <div className="product-detail-tabs__tabs">
+                            {content.fields.sections.map((section, index) => {
+                                return <div key={index} className={`product-detail-tabs__tab ${index == activeSlide ? "active" : ""}`} onClick={() => goToSlide(index)}>{ section.fields.title }</div>
+                            })}
+                        </div>
+                    ) : ""}
                 </div>
-                <Swiper
-                    className="product-detail-tabs__slider"
-                    modules={[EffectFade]}
-                    spaceBetween={20}
-                    slidesPerView={1}
-                    effect={"fade"}
-                    style={{
-                        "--swiper-pagination-color": "#00B188"
-                    }}
-                    onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
-                    onSwiper={(swiper) => setSwiperInstance(swiper)}
-                >
-                    <SwiperSlide>
-                        <ProductDetailSlide activeSlide={activeSlide} />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <ProductDetailSlide activeSlide={activeSlide} />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <ProductDetailSlide activeSlide={activeSlide} />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <ProductDetailSlide activeSlide={activeSlide} />
-                    </SwiperSlide>
-                </Swiper>
+                {content.fields?.sections ? (
+                    <Swiper
+                        className="product-detail-tabs__slider"
+                        modules={[EffectFade]}
+                        spaceBetween={20}
+                        slidesPerView={1}
+                        effect={"fade"}
+                        style={{
+                            "--swiper-pagination-color": "#00B188"
+                        }}
+                        onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+                        onSwiper={(swiper) => setSwiperInstance(swiper)}>
+                        {content.fields.sections.map((section, index) => {
+                            return (
+                                <SwiperSlide key={index}> 
+                                    <ProductDetailSlide activeSlide={activeSlide} content={section} />
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                ) : ""}
             </div>   
         </section>
     )
