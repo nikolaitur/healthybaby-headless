cb5990ce5a01660f6bbead2feb5093ea994c7ebe
nikolaitur <n.turovskyi@gmail.com>
2022-10-07 11:01:39 -0700

    fix article activity hero

--- components/Article/ArticleHeroEverGreen/ArticleHeroEverGreen.js
+++ components/Article/ArticleHeroEverGreen/ArticleHeroEverGreen.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import ReactPlayer from 'react-player/lazy'
 import ShieldPlus from '../../../svgs/ShieldPlus.svg'
 import DoubleWave from '../../../svgs/DoubleWave.js'
 import DoubleWaveMobile from 'svgs/DoubleWaveMobile'
+import PlayIcon from '../../../svgs/play-icon.svg'
 
 const ArticleHeroEverGreen = ({ content }) => {
   // const { ctaText, ctaUrl } = content.fields
   // const backgroundImage = content.fields.image.fields.file.url
+
+  const [hasWindow, setHasWindow] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHasWindow(true)
+    }
+  }, [])
+
+  const playVideo = () => {
+    setIsPlaying(true)
+  }
 
   const getDateFormat = (date) => {
     const monthNames = [
@@ -109,33 +125,76 @@ const ArticleHeroEverGreen = ({ content }) => {
               </div>
             </div>
           </div>
-          <div className="article-hero__image">
-            <div className="article-hero__image--desktop">
-              <Image
-                className=""
-                src={`https:${content.fields.articleHero.fields.featuredMedia.fields.file.url}`}
-                alt={`image`}
-                layout="responsive"
-                objectFit="cover"
-                height="700"
-                width="650"
-              />
+          {content.fields.articleHero.fields.videoLink ? (
+            <div className="article-video">
+              <div
+                className={`article-video__icon ${isPlaying ? 'hide' : ''}`}
+                onClick={() => playVideo()}
+              >
+                <PlayIcon />
+              </div>
+              <div
+                className={`article-video__image ${isPlaying ? 'hide' : ''}`}
+                onClick={() => playVideo()}
+              >
+                <Image
+                  className=""
+                  src={`https:${content.fields.articleHero.fields.featuredMedia.fields.file.url}`}
+                  alt={`video`}
+                  layout="responsive"
+                  objectFit="cover"
+                  height={
+                    content.fields.articleHero.fields.featuredMedia.fields.file
+                      .details.image.height
+                  }
+                  width={
+                    content.fields.articleHero.fields.featuredMedia.fields.file
+                      .details.image.width
+                  }
+                />
+              </div>
+              <div className="article-video__video">
+                {hasWindow && (
+                  <ReactPlayer
+                    url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+                    playing={isPlaying}
+                    controls={true}
+                    className="article-video__video-player"
+                    width="100%"
+                    height="100%"
+                  />
+                )}
+              </div>
             </div>
-            <div className="article-hero__image--mobile">
-              <Image
-                className=""
-                src={`https:${content.fields.articleHero.fields.featuredMediaMobile.fields.file.url}`}
-                alt={`image`}
-                layout="responsive"
-                objectFit="cover"
-                height="700"
-                width="650"
-              />
+          ) : (
+            <div className="article-hero__image">
+              <div className="article-hero__image--desktop">
+                <Image
+                  className=""
+                  src={`https:${content.fields.articleHero.fields.featuredMedia.fields.file.url}`}
+                  alt={`image`}
+                  layout="responsive"
+                  objectFit="cover"
+                  height="700"
+                  width="650"
+                />
+              </div>
+              <div className="article-hero__image--mobile">
+                <Image
+                  className=""
+                  src={`https:${content.fields.articleHero.fields.featuredMediaMobile.fields.file.url}`}
+                  alt={`image`}
+                  layout="responsive"
+                  objectFit="cover"
+                  height="700"
+                  width="650"
+                />
+              </div>
+              <div className="article-hero__image--credit">
+                {content.fields.articleHero.fields.photoCredit}
+              </div>
             </div>
-            <div className="article-hero__image--credit">
-              {content.fields.articleHero.fields.photoCredit}
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
