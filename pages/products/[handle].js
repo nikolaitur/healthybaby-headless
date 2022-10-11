c62f254466be8a4ea1b75d17d1ab100c0906768e
nikolaitur <n.turovskyi@gmail.com>
2022-10-11 11:36:06 -0700

    fix: fixes merge conflict

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -1,10 +1,5 @@
-import { useState, useEffect } from 'react'
-import Image from 'next/image'
-import { useCart } from '@nacelle/react-hooks'
+import { useEffect } from 'react'
 import { nacelleClient } from 'services'
-import { getSelectedVariant } from 'utils/getSelectedVariant'
-import { getCartVariant } from 'utils/getCartVariant'
-import styles from 'styles/Product.module.css'
 import { dataLayerViewProduct } from '@/utils/dataLayer'
 
 import ProductGallery from '../../components/Product/ProductGallery'
@@ -12,66 +7,10 @@ import ProductInfo from '../../components/Product/ProductInfo'
 import ProductSections from '../../components/Product/ProductSections'
 import ProductReviews from '../../components/Product/ProductReviews'
 
-function Product({ product }) {
-  const [, { addToCart }] = useCart()
-  const [selectedVariant, setSelectedVariant] = useState(product.variants[0])
-  const [selectedOptions, setSelectedOptions] = useState(
-    selectedVariant.content.selectedOptions
-  )
-  const [quantity, setQuantity] = useState(1)
-
+function Product({ product, page }) {
   useEffect(() => {
     dataLayerViewProduct({ product })
   }, [])
-
-  let options = null
-  if (product?.content?.options?.some((option) => option.values.length > 1)) {
-    options = product?.content?.options
-  }
-
-  const buttonText = selectedVariant
-    ? selectedVariant.availableForSale
-      ? 'Add To Cart'
-      : 'Sold Out'
-    : 'Select Option'
-
-  const handleOptionChange = (event, option) => {
-    const newOption = { name: option.name, value: event.target.value }
-    const optionIndex = selectedOptions.findIndex((selectedOption) => {
-      return selectedOption.name === newOption.name
-    })
-
-    const newSelectedOptions = [...selectedOptions]
-    if (optionIndex > -1) {
-      newSelectedOptions.splice(optionIndex, 1, newOption)
-      setSelectedOptions([...newSelectedOptions])
-    } else {
-      setSelectedOptions([...newSelectedOptions, newOption])
-    }
-    const variant = getSelectedVariant({
-      product,
-      options: newSelectedOptions,
-    })
-    setSelectedVariant(variant ? { ...variant } : null)
-  }
-
-  const handleQuantityChange = (event) => {
-    setQuantity(+event.target.value)
-  }
-
-  // Get product data and add it to the cart by using `addToCart`
-  // from the `useCart` hook provided by `@nacelle/react-hooks`.
-  // (https://github.com/getnacelle/nacelle-react/tree/main/packages/react-hooks)
-  const handleAddItem = () => {
-    const variant = getCartVariant({
-      product,
-      variant: selectedVariant,
-    })
-    addToCart({
-      variant,
-      quantity,
-    })
-  }
   return (
     product && (
       <section className="product-main">
