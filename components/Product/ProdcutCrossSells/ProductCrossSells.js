aa621a8fef58804ffccb46cb4ba5a8bfe7a8c172
nikolaitur <n.turovskyi@gmail.com>
2022-10-11 09:17:45 -0700

    wip qa

--- components/Product/ProdcutCrossSells/ProductCrossSells.js
+++ components/Product/ProdcutCrossSells/ProductCrossSells.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Lazy, EffectFade } from "swiper";
+
+import 'swiper/css';
+import "swiper/css/lazy";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import CollectionProductCard from '../../Cards/CollectionProductCard'
 
 const ProductCrossSells = ({ content }) => {
@@ -16,11 +25,32 @@ const ProductCrossSells = ({ content }) => {
                     <div className="product-cross-sells__header" dangerouslySetInnerHTML={{__html:  documentToHtmlString(content.fields.header) }}></div>
                 : ""}
                 {content.fields?.sections ? (
-                    <div className="product-cross-sells__items">
-                         {content.fields.sections.map((item, index) => {
-                            return <CollectionProductCard content={item} key={index}/>
-                         })}
-                    </div>
+                    <>
+                        <div className="product-cross-sells__items">
+                            {content.fields.sections.map((item, index) => {
+                                return <CollectionProductCard content={item} key={index}/>
+                            })}
+                        </div>
+                        <Swiper
+                            className="product-cross-sells__slider"
+                            modules={[Lazy]}
+                            spaceBetween={20}
+                            slidesPerView={1}
+                            lazy={true}
+                            
+                            style={{
+                                "--swiper-navigation-color": "#fff",
+                                "--swiper-pagination-color": "#fff",
+                            }}
+                        >
+                            {content.fields.sections.map((item, index) => (
+                                <SwiperSlide>
+                                    <CollectionProductCard content={item} key={index}/>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </>
+
                 ) : ""}
             </div>
         </div>
