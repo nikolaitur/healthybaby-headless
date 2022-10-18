fc4cd40d954ccd5260e81a9fca5d72df03706dad
nikolaitur <n.turovskyi@gmail.com>
2022-10-18 11:23:09 -0700

    feat: adds back ContentSections

---
+++ components/Sections/ContentSections/ContentSections.js
@@ -0,0 +1,56 @@
+import HeroBanner from '../HeroBanner'
+import ContentBanner from '../ContentBanner'
+import CollectionCards from '../CollectionCards'
+import DiaperFinder from '../DiaperFinder'
+import ShopByCategory from '../ShopByCategory'
+import BestSellers from '../BestSellers'
+import StoriesSlider from '../StoriesSlider'
+import About from '../About'
+import WowAndHow from '../WowAndHow'
+import FAQGroup from '../FAQGroup'
+import ContactForm from '../ContactForm'
+import AfterShip from '../AfterShip'
+import JazzHR from '../JazzHR'
+
+const ContentSections = ({ sections }) => {
+    return (
+        <>
+            {sections.map((section, index) => {
+                const type = section.type
+
+                switch (type) {
+                    case 'heroBanner':
+                        return <HeroBanner content={section} key={index} />
+                    case 'contentBanner':
+                        return <ContentBanner content={section} key={index} />
+                    case 'categoryCards':
+                        return <CollectionCards content={section} key={index} />
+                    case 'diaperFinder':
+                        return <DiaperFinder content={section} key={index} />
+                    case 'shopByCategory':
+                        return <ShopByCategory content={section} key={index} />
+                    case 'bestSellers':
+                        return <BestSellers content={section} key={index} />
+                    case 'storiesSlider':
+                        return <StoriesSlider content={section} key={index} />
+                    case 'wowAndHow':
+                        return <WowAndHow content={section} key={index} />
+                    case 'about':
+                        return <About content={section} key={index} />
+                    case 'faqGroup':
+                        return <FAQGroup content={section} key={index} />
+                    case 'contactForm':
+                        return <ContactForm content={section} key={index} />
+                    case 'afterShipWidget':
+                        return <AfterShip content={section} key={index} />
+                    case 'jazzHrWidget':
+                        return <JazzHR content={section} key={index} />
+                    default:
+                    return null
+                }
+            })}
+        </>
+    )
+}
+
+export default ContentSections
