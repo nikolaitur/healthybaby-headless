8d99390057ef32d37c71bc60d4406a3b46500e40
nikolaitur <n.turovskyi@gmail.com>
2022-10-21 12:20:19 -0700

    feat: adds video link support for hp hero

--- components/Sections/HeroBanner/HeroBanner.js
+++ components/Sections/HeroBanner/HeroBanner.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 import { Player } from 'video-react';
 import LongArrowRight from '../../../svgs/long-arrow-right.svg'
 import parse from 'html-react-parser'
+import ReactPlayer from 'react-player/lazy'
+import PlayIcon from '@/svgs/play-icon.svg'
+
 
 const HeroBanner = ({ content }) => {
-    const { title, subtitle, subtitlePosition, featuredMedia, mobileBackgroundImage, ctaText, ctaUrl, alignment, verticalAlignment, hideCta } = {...content.fields}
+
+    console.log("content:", content)
+
+    const { title, subtitle, subtitlePosition, featuredMedia, mobileBackgroundImage, ctaText, ctaUrl, alignment, verticalAlignment, hideCta, videoLink } = {...content.fields}
+
+    const [hasWindow, setHasWindow] = useState(false)
+    const [isPlaying, setIsPlaying] = useState(false)
 
     const isVideo = featuredMedia?.fields?.file?.contentType.includes('video')
 
@@ -16,44 +25,83 @@ const HeroBanner = ({ content }) => {
         subtitleTruePosition = subtitlePosition
     }
 
+    const playVideo = () => {
+        setIsPlaying(true)
+      }
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+          setHasWindow(true)
+        }
+      }, [])
+
     return (
-        <section className={`hero ${isVideo ? 'hero--video' : ''}`}>
+        <section className={`hero ${isVideo || videoLink ? 'hero--video' : ''}`}>
             <div className="hero__image">
 
-                {featuredMedia?.fields?.file && isVideo && <Player playsInline loop={true} autoPlay={true} muted={true}>
-                    <source src={`https:${featuredMedia.fields.file.url}`} />
-                </Player>}
+                {hasWindow && videoLink ? (
+                    <ReactPlayer
+                        url={videoLink}
+                        playing={isPlaying}
+                        controls={true}
+                        className="hero-video__video-player"
+                        width="100%"
+                        height="100%"
+                    />
+                ): (isVideo) && (
+                    featuredMedia?.fields?.file && isVideo && <Player playsInline loop={true} autoPlay={true} muted={true}>
+                        <source src={`https:${featuredMedia.fields.file.url}`} />
+                    </Player>
+                )}
 
-                {featuredMedia?.fields?.file && !isVideo && <Image
-                    className="hero__image--desktop"
-                    src={`https:${featuredMedia.fields.file.url}`}
-                    alt={title}
-                    layout="fill"
-                />}
-                {mobileBackgroundImage?.fields?.file && mobileBackgroundImage.fields.file.contentType.includes('image') && <Image
-                    className="hero__image--mobile"
-                    src={`https:${mobileBackgroundImage.fields.file.url}`}
-                    alt={title}
-                    layout="fill"
-                />}
+
+                {!isPlaying &&
+                    <>
+                        {featuredMedia?.fields?.file && !isVideo && <Image
+                            className="hero__image--desktop"
+                            src={`https:${featuredMedia.fields.file.url}`}
+                            alt={title}
+                            layout="fill"
+                        />}
+                        {mobileBackgroundImage?.fields?.file && mobileBackgroundImage.fields.file.contentType.includes('image') && <Image
+                            className="hero__image--mobile"
+                            src={`https:${mobileBackgroundImage.fields.file.url}`}
+                            alt={title}
+                            layout="fill"
+                        />}
+                    </>
+                }
             </div>
-            <div className={`hero__container hero__container--vertical-${verticalAlignment} hero__container--horizontal-${alignment} container`}>
+            {!isPlaying && <div className={`hero__container hero__container--vertical-${verticalAlignment} hero__container--horizontal-${alignment} container`}>
                 <div className="hero__content">
                     <div className="hero__content-header">
                         {subtitleTruePosition === 'above title' && <h4 className="hero__subheader">{ subtitle }</h4>}
                         <h1 className="hero__header">{ parse(title) }</h1>
                         {subtitleTruePosition === 'below title' && <h4 className="hero__subheader">{ subtitle }</h4>}
                     </div>
-                    {hideCta === 'False' && <div className="hero__cta">
-                        <Link href={ctaUrl || ''}>
-                            <button className="hero__button btn">
-                                <span>{ctaText}</span>
-                                <span><LongArrowRight /></span>
-                            </button>
-                        </Link>
-                    </div>}
+
+                    {videoLink ? (
+                        <button
+                            className={`hero-video__icon ${isPlaying ? 'hide' : ''}`}
+                            onClick={() => playVideo()}
+                        >
+                            <PlayIcon />
+                        </button>
+                    ):(
+                        <>
+                            {hideCta === 'False' && <div className="hero__cta">
+                                <Link href={ctaUrl || ''}>
+                                    <button className="hero__button btn">
+                                        <span>{ctaText}</span>
+                                        <span><LongArrowRight /></span>
+                                    </button>
+                                </Link>
+                            </div>}
+                        </>
+                    )}
+
                 </div>
-            </div>
+            </div>}
         </section>
     )
 }
